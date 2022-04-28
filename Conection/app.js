1fd28cb4c7a3b665d4af6ef3e636c7d9f3f7f4d2
f.coussi <f.coussi01@ufromail.cl>
2022-04-28 15:06:28 -0500

    Se habilita mostrar en interfaz al usuario solicitado

--- Conection/app.js
+++ Conection/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
+const cors = require('cors')
+
 const mysql = require('mysql');
 
 //Create connection to db
 const db = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'password',
-    database : 'db'
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'db'
 });
 
 //Connect
-db.connect((err)=>{
+db.connect((err) => {
     if (err) {
         throw err;
     } else {
@@ -19,26 +21,26 @@ db.connect((err)=>{
 });
 
 const app = express();
+app.use(cors());
 
-app.get('/getusers/:email/:password',(req, res) => {
+app.get('/getusers/:email/:password', (req, res) => {
     // let sql = 'SELECT * FROM db.user;';
     // let sql = 'SELECT * FROM bd.usuario where email="{req}" and password = "password"; ';
     // let sql = `SELECT * FROM user WHERE email="${req}"`;
-    
     let sql = `SELECT * FROM db.user WHERE email="${req.params.email}" AND password="${req.params.password}"`;
 
     console.log("======")
     console.log(sql);
     // console.log(req.params.email);
     console.log("======")
-    
+
     let query = db.query(sql, (err, results) => {
         if (err) throw err;
         console.log(results);
-        res.send(results);
+        res.json(results);
     });
 })
 
-app.listen("3000", ()=> {
+app.listen("3000", () => {
     console.log("server Started on port 3000");
 });
--- VueInjections/injections/src/views/Login.vue
+++ VueInjections/injections/src/views/Login.vue
@@ -1,104 +1,150 @@
 <template>
   <div class="login">
     <body class="body">
-          <form class="campos" @submit.prevent="onSubmit">
-              <div class="col-md-12">
-              <label for="validationDefaultUsername" class="form-label">Username</label>
-              <div class="input-group">
-                  <span class="input-group-text" id="inputGroupPrepend2">@</span>
-                  <input id="email" type="text" class="form-control"  aria-describedby="inputGroupPrepend2" required>
-              </div>
-              </div>
-              <div class="col-md-12">
-              <label for="validationDefault03" class="form-label">Password</label>
-              <input id="password" type="text" class="form-control" required>
-              </div>
-              <div class="col-12">
-              <button class="btn btn-primary" v-on:click="my('Nose')" type="submit" prevent>Submit</button>
-              </div>
-          </form>
-          <div class="areas">
-          <div class="form-group">
-              <label for="exampleFormControlTextarea2">Comando</label>
-              <textarea readonly class="form-control rounded-0" id="exampleFormControlTextarea2"></textarea>
+      <form class="campos" @submit.prevent="onSubmit">
+        <div class="col-md-12">
+          <label for="validationDefaultUsername" class="form-label"
+            >Username</label
+          >
+          <div class="input-group">
+            <span class="input-group-text" id="inputGroupPrepend2">@</span>
+            <input
+              id="email"
+              type="text"
+              class="form-control"
+              aria-describedby="inputGroupPrepend2"
+              required
+            />
           </div>
-          </div>
+        </div>
+        <div class="col-md-12">
+          <label for="validationDefault03" class="form-label">Password</label>
+          <input id="password" type="text" class="form-control" required />
+        </div>
+        <div class="col-12">
+          <button
+            class="btn btn-primary"
+            v-on:click="my('Nose')"
+            type="submit"
+            prevent
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+      <div class="areas">
+        <div class="form-group">
+          <label for="exampleFormControlTextarea2">Comando</label>
+          <textarea
+            readonly
+            class="form-control rounded-0"
+            id="exampleFormControlTextarea2"
+          ></textarea>
+        </div>
+
+        <div style="background:#e9ecef">
+          <label>Resultado petición</label>
+          <b-card class="mt-3">
+            <pre class="m-0">{{ datos }}</pre>
+          </b-card>
+        </div>
+      </div>
     </body>
   </div>
 </template>
 
 <script>
-
 import ApiService from "../services/api.service.js";
-
 export default {
   name: "Login",
-  data(){
-
+  data() {
+    return {
+      datos: {
+        email: "",
+        password: "",
+        name: "",
+        phone: "",
+      },
+    };
   },
-  components: {
-
+  components: {},
+  mounted() {
+    document.getElementById(
+      "exampleFormControlTextarea2"
+    ).value = `" or "1"="1`;
   },
-  mounted(){
-    document.getElementById("exampleFormControlTextarea2").value = `" or "1"="1`; 
-  },
-  methods:{
-    my(){
+  methods: {
+    my() {
       var email = document.getElementById("email").value;
       var password = document.getElementById("password").value;
-      
-      this.verificarString(email, password); 
-      // this.getUsers(email,password);
+
+      //this.verificarString(email, password);
+      this.getUsers(email, password);
     },
-    getUsers(email,password){
-      ApiService.getUsers(email,password).then(({ data }) => {
-        for (let i = 0; i < data.length; i++) {
-          console.log("Entraste al sistema");
-        }
-        this.isMounted = true;
-      });
+    getUsers(email, password) {
+      ApiService.getUsers(email, password)
+        .then((response) => {
+          this.datos = response.data;
+        })
+        .catch(function (error) {
+          console.log("PROBLEMA EN LA CONEXIÓN: " + error);
+        });
+
+      // axios
+      //   .get(`http://localhost:3000/getusers/${email}/${password}`)
+      //   .then((response) => {
+      //     this.datos = response.data;
+      //   })
+      //   .catch(function (error) {
+      //     console.log("PROBLEMA EN LA CONEXIÓN: " + error);
+      //   });
     },
-    verificarString(email, password){
+    verificarString(email, password) {
       var testEmail = / /.test(email);
       var testPassword = / /.test(password);
-        if (testEmail || testPassword) {
-          alert("Datos Ingresados no validos");
-        } else {
-          this.getUsers(email,password);
-        }    
-    }
-  }
+      if (testEmail || testPassword) {
+        alert("Datos Ingresados no validos");
+      } else {
+        ApiService.getUsers(email, password)
+          .then((response) => {
+            this.datos = response.data;
+          })
+          .catch(function (error) {
+            console.log("PROBLEMA EN LA CONEXIÓN: " + error);
+          });
+      }
+    },
+  },
 };
-
 </script>
 <style>
-.body{
-display: flex;
-flex-direction: row;
+.body {
+  display: flex;
+  flex-direction: row;
 }
-.campos{
-    display: flex;
-    flex-direction: column;
-    padding-left: 20%;
-    padding-top: 5%;
-    gap: 25px;
+.campos {
+  display: flex;
+  flex-direction: column;
+  padding-left: 20%;
+  padding-top: 5%;
+  gap: 25px;
 }
-.btn{
-    margin-top: 1%;
+.btn {
+  margin-top: 1%;
 }
-    .areas{
-        display: flex;
-        flex-direction: column;
-        gap: 60px;
-        padding-left: 5%;
-        padding-top: 5%;
+.areas {
+  display: flex;
+  flex-direction: column;
+  gap: 60px;
+  padding-left: 5%;
+  padding-top: 5%;
 }
-    #exampleFormControlTextarea3{
-        width: 500px;
-        height: 180px;
+#exampleFormControlTextarea3 {
+  width: 500px;
+  height: 180px;
 }
-  #exampleFormControlTextarea2{
-   width: 500px;
-   height: 40px;
+#exampleFormControlTextarea2 {
+  width: 500px;
+  height: 40px;
 }
 </style>
